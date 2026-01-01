dff8794bd22a7b67e3241c3aa26ec5d5187a938f
mariojui200530912 <mariojui200530912@cunoc.edu.gt>
2026-01-01 11:03:53 -0600

    Se arreglo navbar conforme a las rutas

--- ProyectoFrontend/src/app/pages/home/home.component.ts
+++ ProyectoFrontend/src/app/pages/home/home.component.ts
@@ -1,31 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { OnInit, inject } from '@angular/core';
 import { JuegoService } from '../../services/juego.service';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { UsuarioService } from '../../services/usuario.service';
 import { Juego } from '../../models/juego/Juego';
+import { FormsModule } from '@angular/forms';
+import { EmpresaService } from '../../services/empresa.service';
 
 @Component({
   selector: 'app-home.component',
-  imports: [CurrencyPipe, RouterModule, RouterLink],
+  imports: [CurrencyPipe, RouterModule, RouterLink, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   public juegoService = inject(JuegoService);
   public cartService = inject(CartService);
+  public usuarioService = inject(UsuarioService);
+  public empresaService = inject(EmpresaService);
+
+  searchTerm = signal<string>('');
+  searchEmpresa = signal<string>('');
+
+  usuariosFiltrados = computed(() => {
+    const termino = this.searchTerm().toLowerCase();
+    const usuarios = this.usuarioService.usuariosGamer(); // Asumiendo que tienes esta signal en el servicio
+
+    if (!termino) return usuarios;
+
+    return usuarios.filter(u => 
+      u.nickname.toLowerCase().includes(termino) || 
+      (u.pais && u.pais.toLowerCase().includes(termino))
+    );
+  });
+
+  empresasFiltradas = computed(() => {
+    const termino = this.searchEmpresa().toLowerCase();
+    const empresas = this.empresaService.empresas(); // Asumiendo que llenamos esta signal
+
+    if (!termino) return empresas;
+
+    return empresas.filter(e => 
+      e.nombre.toLowerCase().includes(termino) || 
+      (e.descripcion && e.descripcion.toLowerCase().includes(termino))
+    );
+  });
 
   ngOnInit(): void {
     // Al cargar la página, pedimos los datos
     this.juegoService.cargarInicio();
-  }
-
-  // Helper para manejar imágenes base64 si no traen el prefijo
-  getImagenSrc(base64: string): string {
-    if (!base64) return 'assets/images/placeholder.jpg'; // Imagen por defecto
-    if (base64.startsWith('data:')) return base64;
-    return `data:image/jpeg;base64,${base64}`;
+    this.usuarioService.cargarUsuariosGamer();
+    this.empresaService.cargarEmpresas();
   }
 
   agregarAlCarrito(juego: Juego) {
